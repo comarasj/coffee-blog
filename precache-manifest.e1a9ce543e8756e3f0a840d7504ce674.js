dbaa2e46942909be59fae44ca14f09c21257e511
comarasj <36525394+comarasj@users.noreply.github.com>
2018-11-12 19:23:37 -0500

    Updates

---
+++ precache-manifest.e1a9ce543e8756e3f0a840d7504ce674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "2bd29b2c4cbcc056eb5a",
+    "url": "/my-app/static/js/main.2bd29b2c.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/my-app/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "2bd29b2c4cbcc056eb5a",
+    "url": "/my-app/static/css/main.e5350de6.chunk.css"
+  },
+  {
+    "revision": "eccf773bd7fff195f962aa2d8d2c373f",
+    "url": "/my-app/index.html"
+  }
+];
