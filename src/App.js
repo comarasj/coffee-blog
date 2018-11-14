a7d07bd585ef45c2485d41848c58201e762c037d
comarasj <36525394+comarasj@users.noreply.github.com>
2018-11-13 21:12:14 -0500

    edited app name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Steve Comarata's web app.
+          </p>
+          <p>
+            Work in Progress.
           </p>
           <a
             className="App-link"
