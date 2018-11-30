17af13b8f79e4ad9d285f35dd462742cf0bab265
comarasj <comarasj@mail.uc.edu>
2018-11-29 22:03:21 -0500

    Removed react link

--- src/comps/home/home.js
+++ src/comps/home/home.js
@@ -2,30 +2,22 @@ import React, { Component } from 'react';
 import logo from './../../logo.svg';
 import './home.css';
 
-class Home extends Component{
-    render(){
+class Home extends Component {
+    render() {
         return (
             <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Steve Comarata's web app.
-              </p>
-              <p>
-                Work in Progress.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Steve Comarata's web app.
+                    </p>
+                    <p>
+                        Work in Progress.
+                    </p>
+                </header>
+            </div>
         )
     }
-} 
+}
 
 export default Home;
