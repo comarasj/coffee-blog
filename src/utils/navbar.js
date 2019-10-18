92b635e959bb7865e62fbfe33ae5c448d8b657ca
Stephen Comarata <comarasj@mail.uc.edu>
2019-10-18 15:08:12 -0400

    added styling to navbar

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
 class NavBar extends Component {
     render() {
         return (
             <nav>
                 <div className="topbar">
-                    <Link to="/" className="links">Home</Link>
-                    <Link to="/blog" className="links">Blog</Link>
+                    <NavLink to="/home" className="links" activeStyle={{
+                        textDecoration: "underline",
+                        color: "whitesmoke"
+                    }}>Home</NavLink>
+                    <NavLink to="/blog" className="links" activeStyle={{
+                        textDecoration: "underline",
+                        color: "whitesmoke"
+                    }}>Blog</NavLink>
                 </div>
             </nav>
         )
